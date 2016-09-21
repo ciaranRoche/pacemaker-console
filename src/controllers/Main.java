f464e8c6bbd93b393ddfa747df47859e9d8cea4e
ciaran <ciaranroche@hotmail.co.uk>
2016-09-21 09:00:29 +0100

    controller package and main class

---
+++ src/controllers/Main.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+	public static void main(String[] args){
+		List<User>users=new ArrayList<User>();
+	    users.add(new User("Bart", "Simpson", "bart@simpson.com", "secret"));
+	    users.add(new User("Homer", "Simpson", "bart@simpson.com", "secret"));
+	    users.add(new User("Lisa", "Simpson", "bart@simpson.com", "secret"));
+		System.out.println(users);
+	}
+
+}
